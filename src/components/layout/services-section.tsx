fb5fb94144a25f6b0273d03b3b807f2666d3676a
Douglas Vini <douglasdannuz@gmail.com>
2025-09-05 18:24:49 +0000

    meu unico probela está no explore da pagin ados clients que não fica res

--- src/components/layout/services-section.tsx
+++ src/components/layout/services-section.tsx
@@ -38,7 +38,7 @@ export const ServicesSection = () => (
             <h2 className="text-3xl md:text-4xl font-bold font-headline">Nossos Serviços</h2>
             <p className="text-lg text-muted-foreground max-w-2xl mx-auto mt-4">Soluções completas para garantir que sua empresa tenha uma presença digital poderosa e eficaz.</p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
             {services.map(service => (
                 <Card key={service.title} className="flex flex-col text-center items-center bg-white/20 backdrop-blur-md border border-white/10 shadow-lg rounded-2xl">
                     <CardHeader>
