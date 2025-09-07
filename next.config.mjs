db875af0b9302b5945e891afcf49672fb473eab1
Douglas Vini <douglasdannuz@gmail.com>
2025-09-07 21:17:27 +0000

    oque é isso ? 307 charge.updated 17:52:41 307 charge.succeeded 17:52:39

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-                port: '',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'darkgreen-lark-741030.hostingersite.com',
-                port: '',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'assets.zyrosite.com',
-                port: '',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                port: '',
-                pathname: '**',
-            }
-        ],
-    },
-    output: 'standalone', // Isso gera uma versão otimizada para produção
-    swcMinify: true,
-    compress: true,
+  reactStrictMode: true,
+  trailingSlash: false,
 };
 
 export default nextConfig;
