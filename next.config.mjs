27284df17b976194ead8dfc15b58fcc8531d79b0
Douglas Vini <douglasdannuz@gmail.com>
2025-09-07 21:21:14 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'darkgreen-lark-741030.hostingersite.com',
-        port: '',
-        pathname: '/img/**',
-      },
-    ],
-  },
-  trailingSlash: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'darkgreen-lark-741030.hostingersite.com',
+                port: '',
+                pathname: '/img/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'assets.zyrosite.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
