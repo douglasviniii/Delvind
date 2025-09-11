c19d9eb213b2f5fc23b1e96927facc5353559912
Douglas Vini <douglasdannuz@gmail.com>
2025-09-11 17:56:35 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,18 +1,20 @@
 
 import * as admin from 'firebase-admin';
-import serviceAccount from '../../../firebase-service-account.json';
+import serviceAccount from '../../firebase-service-account.json';
 
 // Esta função garante que o SDK do Firebase Admin seja inicializado apenas uma vez.
 export function initializeAdminApp() {
   if (admin.apps.length > 0) {
-    return;
+    // Retorna a instância existente se já foi inicializada
+    return admin.app();
   }
 
   try {
     const credential = admin.credential.cert(serviceAccount as admin.ServiceAccount);
-    admin.initializeApp({ credential });
+    return admin.initializeApp({ credential });
   } catch (error: any) {
     console.error('Falha na inicialização do Firebase Admin SDK.', error);
+    // Lançar um erro aqui pode ajudar a diagnosticar problemas de inicialização
     throw new Error('Could not initialize Firebase Admin SDK: ' + error.message);
   }
 }
