ceefee73be8c9227b54a59fd05026778393cbfbe
Douglas Vini <douglasdannuz@gmail.com>
2025-09-12 14:35:29 +0000

    colocar esse arquivo direto nocodigfo sme depédnerd e um avarivel O arqu

--- src/lib/firebase-admin-init.ts
+++ src/lib/firebase-admin-init.ts
@@ -2,6 +2,13 @@
 import * as admin from 'firebase-admin';
 import serviceAccount from '../../../firebase-service-account.json';
 
+// Converte o objeto de conta de serviço para o tipo esperado pelo Firebase Admin SDK
+const typedServiceAccount = {
+  projectId: serviceAccount.project_id,
+  clientEmail: serviceAccount.client_email,
+  privateKey: serviceAccount.private_key,
+};
+
 export function initializeAdminApp() {
   // Se o app 'admin' já estiver inicializado, retorne-o
   const existingApp = admin.apps.find(app => app?.name === 'admin');
@@ -9,13 +16,10 @@ export function initializeAdminApp() {
     return existingApp;
   }
 
-  // As credenciais são importadas do arquivo JSON
-  const credential = admin.credential.cert(serviceAccount);
-
   try {
     // Inicializa o app com as credenciais do arquivo importado
     return admin.initializeApp({
-      credential,
+      credential: admin.credential.cert(typedServiceAccount),
       storageBucket: 'venda-fcil-pdv.appspot.com',
     }, 'admin'); // Nomeia a instância para evitar conflitos
 
@@ -25,7 +29,7 @@ export function initializeAdminApp() {
   }
 }
 
-// Para manter compatibilidade com o webhook do Stripe
+// Para manter compatibilidade, caso outra parte do sistema use esta função
 export function getAdminApp() {
     return { db: admin.firestore(initializeAdminApp()) };
 }
