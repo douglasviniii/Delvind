585a0c5063cdcd5c840dc59a648f6e253847cc33
Douglas Vini <douglasdannuz@gmail.com>
2025-09-13 11:39:51 +0000

    Procurar Procurar
    set 13, 08:36 - 08:39
    GMT-3
    
    Menu
    
    ==> Baixando cache

--- src/@types/react-rating-stars-component/index.d.ts
+++ src/@types/react-rating-stars-component/index.d.ts
@@ -1,6 +1,18 @@
+
 declare module 'react-rating-stars-component' {
   import React from 'react';
 
-  const ReactStars: React.FC<any>;
+  interface ReactStarsProps {
+    count: number;
+    onChange?: (newRating: number) => void;
+    size?: number;
+    isHalf?: boolean;
+    value?: number;
+    edit?: boolean;
+    activeColor?: string;
+    [key: string]: any; // Allow other props
+  }
+
+  const ReactStars: React.FC<ReactStarsProps>;
   export default ReactStars;
 }
