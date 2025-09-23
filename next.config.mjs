d43a0d82d725688821e163021e5901ebe53e0809
Douglas Vini <douglasdannuz@gmail.com>
2025-09-23 13:00:07 +0000

    QUE SUGNOIFICA ISSO? @@ -1,3 +1,4 @@
    /** @type {import('next').NextConf

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+  },
   images: {
     remotePatterns: [
       {
@@ -11,21 +15,22 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/venda-fcil-pdv.appspot.com/**',
       },
       {
         protocol: 'https',
         hostname: 'assets.zyrosite.com',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
-      }
+        port: '',
+        pathname: '/**',
+      },
     ],
-  },
-  env: {
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
-    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
   },
 };
 
