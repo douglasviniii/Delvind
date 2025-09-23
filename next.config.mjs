7f5ebd62b4420254d9e6b4849c12afcd77bbb6cf
Douglas Vini <douglasdannuz@gmail.com>
2025-09-23 12:37:33 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,12 @@ const nextConfig = {
         port: '',
         pathname: '/img/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/**',
+      },
     ],
   },
 };
