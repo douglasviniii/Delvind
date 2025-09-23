591cb3459f6965a57948c4f55cfcd5ebeb167438
Douglas Vini <douglasdannuz@gmail.com>
2025-09-23 12:47:10 +0000

    ele não consegue ler ainda e dá isso como s enão tivesse.. mas ta tyuo l

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,16 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'darkgreen-lark-741030.hostingersite.com',
+        port: '',
+        pathname: '/img/**',
       },
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        hostname: 'firebasestorage.googleapis.com',
       },
       {
         protocol: 'https',
@@ -21,9 +18,14 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
+        hostname: 'lh3.googleusercontent.com',
+      }
     ],
+  },
+  env: {
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
   },
 };
 
